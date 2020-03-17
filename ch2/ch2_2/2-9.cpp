a23caced39c494eee777f53fd1c3225d4aa78989
jxdeng <yuluowuhen007@qq.com>
2020-03-17 16:25:22 +0800

    common commit

---
+++ ch2/ch2_2/2-9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+string global_str;
+int global_int;
+int main()
+{
+    string local_str;
+    int local_int, i(3.14);
+    cout << " global_str=" << global_str << " local_str=" << local_str << endl;
+    cout << " global_int=" << global_int << " local_int=" << local_int << " i=" << i << endl;
+    return 0;
+}
--- test/helloworld.cpp
+++ test/helloworld.cpp
@@ -3,13 +3,21 @@
 #include <string>
 
 using namespace std;
-
 int main()
 {
    vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
    for (const string &word : msg)
    {
-      cout << word << " ";
+      cout << word << "abc"
+      "def "<<endl;
    }
    cout << endl;
+   /*    for (unsigned u = 5; u >0; --u)
+   {
+      cout << u << endl;
+   } */
+   unsigned char u = -1;
+   cout << u << endl;
+   int i(3.14);
+   cout << i << endl;
 }
