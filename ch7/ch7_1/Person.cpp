2e2a29ff70670bafe88f07d64f06c0a1e6168f24
jxdeng <yuluowuhen007@qq.com>
2020-06-24 17:08:34 +0800

    ch7_1 1

--- ch7/ch7_1/Person.cpp
+++ ch7/ch7_1/Person.cpp
@@ -8,6 +8,27 @@ private:
     string address;
 
 public:
+    Person() = default;
+    Person(const string &na, const string &add) : name(na), address(add) {}
+    Person(istream &is);
+
+public:
     string getName() const { return name; }
     string getAddress() const { return address; }
+    friend istream &read(istream &is, Person &person);
+    friend ostream &print(ostream &os, const Person &person);
 };
+
+Person::Person(istream &is) { read(is, *this); }
+
+istream &read(istream &is, Person &person)
+{
+    is >> person.name >> person.address;
+    return is;
+}
+
+ostream &print(ostream &os, const Person &person)
+{
+    os << person.name << " " << person.address << endl;
+    return os;
+}
